3844c0f7dbcd9e71c222cf077e6a4c097d903a7f
Viraj Rathod <virajrathod9139@gmail.com>
2024-05-29 17:21:06 +0530

    Update Connection.js

--- connection/Connection.js
+++ connection/Connection.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 
 
-mongoose.connect("mongodb+srv://Viru:viru123@cluster0.xt4zi9h.mongodb.net/E-Sports-Arena-API", { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect("", { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => {console.log("DataBase is ConneCted........!")})
 .catch((err) => {console.log(err.message)})
